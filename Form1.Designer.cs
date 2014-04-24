489466a3f5bd2c7a1af7252dd079d5e161a87821
Fatih Durmuş <fatihdurmus15@yahoo.com>
2014-04-24 13:40:13 +0300

    Beta1
    Şu an için sadece wordpress ve joomla tespit ediyor ve bir sürü bug var

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace jw1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("");
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtSelect = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.adres = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.turu = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(242, 13);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Aç";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtSelect
+            // 
+            this.txtSelect.Location = new System.Drawing.Point(13, 13);
+            this.txtSelect.Name = "txtSelect";
+            this.txtSelect.Size = new System.Drawing.Size(223, 20);
+            this.txtSelect.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.adres,
+            this.turu});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            listViewItem1.StateImageIndex = 0;
+            listViewItem2.StateImageIndex = 0;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2});
+            this.listView1.Location = new System.Drawing.Point(13, 40);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(761, 243);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // adres
+            // 
+            this.adres.Width = 108;
+            // 
+            // turu
+            // 
+            this.turu.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 295);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.txtSelect);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtSelect;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader adres;
+        private System.Windows.Forms.ColumnHeader turu;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using System.Net;
+
+namespace jw1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        WebBrowser mywebBrowser;
+        string url;
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            string file="";
+            string text = "";
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                file = openFileDialog1.FileName;
+                text = File.ReadAllText(file);
+                size = text.Length;
+            }
+            StreamReader reader = new StreamReader(file);
+            while ((text = reader.ReadLine())!=null)
+            {
+                MessageBox.Show(text.ToString());
+                var something = DetectWordpress(text);
+                
+
+            }
+        }
+        protected string DetectJoomla(string url)
+        {
+            string system="";
+            HtmlWeb client = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = client.Load(url);
+            HtmlNodeCollection Nodes = doc.DocumentNode.SelectNodes("//a[@href]");
+            int dongu = 0;
+            foreach (var link in Nodes)
+            {
+                if (link.Attributes["href"].Value.Contains("joomla") && dongu < 1)
+                {
+                    dongu += 1;
+                    listView1.Items.Add(url + " joomla bir site");
+                }
+                
+            }
+            return system;
+        }
+        protected string DetectWordpress(string url)
+        {
+            string system="bos";
+            string html;
+            using (WebClient client = new WebClient())
+            {
+                html = client.DownloadString(url);
+            }
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            foreach (HtmlNode img in doc.DocumentNode.SelectNodes("//img"))
+            {
+                if (img.OuterHtml.Contains("wp-content"))
+                {
+                    listView1.Items.Add(url+" Wordpress bir site");
+                }
+                else
+                {
+                    var anotherthing = DetectJoomla(url);
+
+                }
+            }
+
+
+            return system;
+            }
+
+       
+        }
+       
+        }
+
